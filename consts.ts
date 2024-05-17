9149bad4833cb9a4d447221aa99d53d60c411e4f
YSL4L <88391340+YSL4L@users.noreply.github.com>
2024-05-18 01:50:54 +0200

    add .env.example; add constants in root

---
+++ consts.ts
@@ -0,0 +1,10 @@
+const config = {
+    piholeApiUrl: process.env.PIHOLE_API_URL,
+    email: {
+        ADRESS: process.env.EMAIL_ADDRESS,
+        PASSWORD: process.env.EMAIL_PASSWORD,
+        SMTPSERVER: process.env.SMTP_SERVER,
+       SMTPPORT: process.env.SMTP_PORT
+    },
+    RECEPIENTEMAIL: process.env.RECIPIENT_EMAIL
+};
